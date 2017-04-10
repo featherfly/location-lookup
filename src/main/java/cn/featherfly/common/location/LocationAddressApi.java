6e85d9e15768a18d3830d74d84d92277ca2fc140
zhongji <zhongji@9imeihuo.com>
2017-04-10 16:21:04 +0800

    整理gradle构建文件

--- src/main/java/cn/featherfly/common/location/LocationAddressApi.java
+++ src/main/java/cn/featherfly/common/location/LocationAddressApi.java
@@ -10,6 +10,7 @@ public interface LocationAddressApi<T, L extends LocationAddress> {
      * 获取目标信息对应的地址信息
      * @param target 目标信息
      * @return 地址信息
+     * @throws Exception 异常
      */
     L getLocationAddress(T target) throws Exception;
 }
